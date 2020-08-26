362f786298d1f1b3bf5a41a33c3d7553299cba48
Beau Sherman <brsherman88@gmail.com>
2020-08-26 16:41:15 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+  $('body').removeClass('preload');
+  
+  $('.main__carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 670,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  })
+})
